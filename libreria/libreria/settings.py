c7561fc15a5f772e34c6edea0bf980886a642bd3
Carlos-CodeBot <83562533+Carlos-CodeBot@users.noreply.github.com>
2022-09-01 07:03:07 -0500

    1.5

--- libreria/libreria/settings.py
+++ libreria/libreria/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-u^r8e6p#fxk65!p2$-i^-u+_y@g4)3j)n2bpt3nc=!j_+3=mmh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -120,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#LOGIN CONFIGURATION
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
